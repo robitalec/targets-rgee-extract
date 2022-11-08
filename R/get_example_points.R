01e0338b1112e4a6adb3753799ec5bcbd7aa4cc5
Alec L. Robitaille <robit.alec@gmail.com>
2022-11-07 22:30:30 -0800

    fix example points

--- R/get_example_points.R
+++ R/get_example_points.R
@@ -10,8 +10,8 @@ get_example_points <- function(x, y, buffer_size, n_pts) {
 	xy_buff <- st_buffer(xy_sf, buffer_size)
 
 	xy_sample <- st_sample(xy_buff, n_pts)
-	out_sf <- st_as_sf(xy_sample)
-	out_sf$id <- seq.int(nrow(out_sf))
+	out <- data.frame(id = seq.int(n_pts))
+	out$geometry <- xy_sample
 
-	return(out_sf)
+	return(st_as_sf(out))
 }
