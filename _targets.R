b81e316d3ebbadfb2a52f02392b83ddd463cfc8b
Alec L. Robitaille <robit.alec@gmail.com>
2022-11-07 21:45:55 -0800

    eg data origin

--- _targets.R
+++ _targets.R
@@ -17,14 +17,18 @@ n_pts <- 1e3
 
 
 # Data --------------------------------------------------------------------
-get_fake_points <- function(x, y, buffer_size) {
-	xy_pt <- st_point(c(-68.3559, 49.4672))
-	xy_df <- data.frame(id = 'mid')
-	xy_df$geom <- st_sfc(xy_pt)
-	xy_sf <- st_as_sf(xy_df, crs = 4326)
-	xy_buff <- st_buffer(xy_sf, 100)
+# There are a number of ways one might end up with a dataset of locations
+#   to extract through rgee / Earth Engine
+# Here are some examples:
 
-}
+# 1) Load data files eg. a CSV of points
+points_csv <- fread('data/example-points.csv')
+
+# 2) Points originating from some other function
+# (See targets_setup/points in buffer)
+
+
+
 
 # Targets: setup ----------------------------------------------------------
 targets_setup <- c(
