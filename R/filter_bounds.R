990219a67e1d6eb383683c5c80c9c8244e0fe5bc
Alec L. Robitaille <robit.alec@gmail.com>
2022-11-09 15:09:19 -0330

    add docs filter bounds

--- R/filter_bounds.R
+++ R/filter_bounds.R
@@ -1,3 +1,7 @@
+#' Filter image collection using bounds of a provided region (feature)
+#' @return
+#' @author Alec L. Robitaille
+#' @export
 filter_bounds <- function(image_collection, region) {
 	image_collection |>
 		ee$ImageCollection$filterBounds(sf_as_ee(st_as_sfc(st_bbox(region))))
