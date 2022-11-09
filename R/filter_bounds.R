6fd456e9c002182aec687f75f028a5f67c47f054
Alec L. Robitaille <robit.alec@gmail.com>
2022-11-09 15:05:41 -0330

    fix arg

--- R/filter_bounds.R
+++ R/filter_bounds.R
@@ -1,4 +1,4 @@
 filter_bounds <- function(image_collection, region) {
 	image_collection |>
-		ee$ImageCollection$filterBounds(sf_as_ee(st_as_sfc(st_bbox(features))))
+		ee$ImageCollection$filterBounds(sf_as_ee(st_as_sfc(st_bbox(region))))
 }
