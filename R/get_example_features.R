2e7328418473ac400f2553c4bee8283bc3fb57b4
Alec L. Robitaille <robit.alec@gmail.com>
2022-11-09 13:09:26 -0330

    rename to get example features

---
+++ R/get_example_features.R
@@ -0,0 +1,18 @@
+#' Get example features
+#' @return
+#' @param diameter size of example study region's (circle) diameter
+#' @param type either 'points', or 'polygons'
+#' @author Alec L. Robitaille
+#' @export
+get_example_features <- function(x, y, type = 'points', diameter, n_features) {
+	xy_pt <- st_point(c(x, y))
+	xy_buff <- st_buffer(xy_pt, buffer_size)
+
+	xy_sample <- st_sample(xy_buff, n_pts)
+	out <- data.frame(id = seq.int(n_pts))
+	out$geometry <- xy_sample
+
+	out_point <- st_as_sf(out, crs = 4326)
+
+	return()
+}
