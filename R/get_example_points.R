8fd6947a1c49cb54f440e7c4dd52035379b29e57
Alec L. Robitaille <robit.alec@gmail.com>
2022-11-09 13:05:21 -0330

    simpler example points

--- R/get_example_points.R
+++ R/get_example_points.R
@@ -4,14 +4,11 @@
 #' @export
 get_example_points <- function(x, y, buffer_size, n_pts) {
 	xy_pt <- st_point(c(x, y))
-	xy_df <- data.frame(id = 'mid')
-	xy_df$geom <- st_sfc(xy_pt)
-	xy_sf <- st_as_sf(xy_df, crs = 4326)
-	xy_buff <- st_buffer(xy_sf, buffer_size)
+	xy_buff <- st_buffer(xy_pt, buffer_size)
 
 	xy_sample <- st_sample(xy_buff, n_pts)
 	out <- data.frame(id = seq.int(n_pts))
 	out$geometry <- xy_sample
 
-	return(st_as_sf(out))
+	return(st_as_sf(out, crs = 4326))
 }
